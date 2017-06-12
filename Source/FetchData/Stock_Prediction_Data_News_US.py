e442e2e75179afbb783b7e1f2c47951aad85f879
yangyue <bluelight598@hotmail.com>
2017-06-12 11:46:40 +0800

    add differential update logic

--- Source/FetchData/Stock_Prediction_Data_News_US.py
+++ Source/FetchData/Stock_Prediction_Data_News_US.py
@@ -3,8 +3,9 @@ import os, configparser
 import pandas as pd
 from eventregistry import *
 from googletrans import Translator
+from Stock_Prediction_Data_Stock_US import getStocksList
 
-def updateNewsArticle(er, dir, stock, from_date, till_date, count):
+def getSingleStockNewsArticle(er, stock, from_date, till_date, count):
     start = from_date.split('-')
     start_y, start_m, start_d = int(start[0]), int(start[1]), int(start[2]) # starting date
 
@@ -21,16 +22,11 @@ def updateNewsArticle(er, dir, stock, from_date, till_date, count):
             categories = True, location = True, image = True))))
     res = er.execQuery(q)
 
+    df = pd.DataFrame(columns=['date', 'title', 'source', 'body', 'uri'])
+
     if 'info' in res:
-        print(res["info"])
-        return
-
-    if os.path.exists(dir) == False: 
-        os.makedirs(dir)
-    
-    filename = dir + stock + '.csv'
-
-    df = getNewsDataFrame(filename, stock)
+        print(stock, res["info"])
+        return df
 
     #translator = Translator()
     #count = 1
@@ -48,24 +44,80 @@ def updateNewsArticle(er, dir, stock, from_date, till_date, count):
         #     trans = translator.translate(line, src='en', dest='zh-CN').text
         #     print(line + "\n\n" + trans + "\n")
         #count += 1
+    return df
     
-    df.drop_duplicates(subset=['uri'], keep=False)
-    df.set_index(['date'], inplace=True)
-    df.sort_index(ascending=True, inplace=True)
-    df.to_csv(filename)
+    
 
-def getNewsDataFrame(dir, stock):
+def updateNewsArticle(er, dir, stock, from_date, till_date, count):
+    if os.path.exists(dir) == False: 
+        os.makedirs(dir)
+    
+    filename = dir + stock + '.csv'
+
+    requireDownload = False
+
     try:
-        return pd.read_csv(filename)
+        df = pd.read_csv(filename)
     except:
-        return pd.DataFrame(columns=['date', 'title', 'source', 'body', 'uri'])
+        requireDownload = True
+
+    if df.empty:
+        requireDownload = True
+
+    if requireDownload:
+        df = getSingleStockNewsArticle(er, stock, from_date, till_date, count)
+        df.set_index(['date'], inplace=True)
+        df.sort_index(ascending=True, inplace=True)
+        df.to_csv(filename)
+        return 
+        
+    first_date = pd.Timestamp(df['date'].iloc[0]).tz_localize(None)
+    last_date  = pd.Timestamp(df['date'].iloc[-1]).tz_localize(None)
+
+    modified = False
+
+    # require pre download
+    if first_date > pd.Timestamp(from_date):
+        pre_df = getSingleStockNewsArticle(er, stock, from_date, first_date.strftime("%Y-%m-%d"), count)
+        print("pre_df", from_date, first_date.strftime("%Y-%m-%d"))
+        print(pre_df)
+        df = pd.concat([pre_df, df])
+        modified = True
+    
+    if last_date < pd.Timestamp(till_date):
+        post_df = getSingleStockNewsArticle(er, stock, last_date.strftime("%Y-%m-%d"), till_date, count)
+        print("post_df", last_date.strftime("%Y-%m-%d"), till_date)
+        print(post_df)
+        df = pd.concat([df, post_df])
+        modified = True
+
+    if modified:
+        df.drop_duplicates(subset=['uri'], keep=False)
+        df.set_index(['date'], inplace=True)
+        df.sort_index(ascending=True, inplace=True)
+        print("modified", df)
+        df.to_csv(filename)
 
     
 if __name__ == "__main__":
+    pd.set_option('precision', 3)
+    pd.set_option('display.width',1000)
+    warnings.filterwarnings('ignore', category=pd.io.pytables.PerformanceWarning)
+
     Config = configparser.ConfigParser()
     Config.read("../../config.ini")
     dir = Config.get('Paths', 'NEWS_US')
 
+    #stocklist = getStocksList()
+    stocklist = ['AMD', 'WDC', 'SINA', 'WB', 'CTRP', 'NTES', 'ATVI', 'FB', 'GLUU', 'NVDA', 'NFLX', 'GPRO',
+                 'MRVL', 'SMCI', 'JD', 'INTC', 'AMZN', 'BIDU', 'BGNE', 'QIWI', 'XNET', 'MOMO', 'YY']
+
+
     er = EventRegistry(apiKey = Config.get('EventRegistry', 'KEY'))
-    updateNewsArticle(er, dir, "GPRO", "2017-06-01", "2017-06-10", 100)
+    now = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    updateNewsArticle(er, dir, 'NTES', "2017-06-01", now, 200)
+
+    # for symbol in stocklist:
+    #     updateNewsArticle(er, dir, symbol, "2017-05-01", now, 200)
     
