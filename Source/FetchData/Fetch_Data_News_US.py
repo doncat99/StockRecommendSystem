2a456ded4a76713bacf4d09fb94a16d668e114b5
yangyue <bluelight598@hotmail.com>
2017-06-29 00:22:19 +0800

    bugfix

--- Source/FetchData/Fetch_Data_News_US.py
+++ Source/FetchData/Fetch_Data_News_US.py
@@ -29,7 +29,7 @@ def getSingleStockNewsArticle(root_path, stock, from_date, till_date, count):
     end_y, end_m, end_d = int(end[0]), int(end[1]), int(end[2]) # until now
 
     q = QueryArticles(lang='eng')
-    #q.setDateLimit(datetime.datetime(start_y, start_m, start_d), datetime.datetime(end_y, end_m, end_d))
+    q.setDateLimit(datetime.datetime(start_y, start_m, start_d), datetime.datetime(end_y, end_m, end_d))
     q.addKeyword(stock)
     q.addRequestedResult(RequestArticlesInfo(count = count, 
     returnInfo = ReturnInfo(
